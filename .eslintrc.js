0a8ff8f757e45cfef0531e9db62d9157b1d64a4f
FooQoo <horseshoes.buzz.17@gmail.com>
2022-08-06 14:04:32 +0900

    remove prettier/@typescript-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,6 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:jsx-a11y/recommended",
     "plugin:prettier/recommended",
-    "prettier/@typescript-eslint",
   ],
   rules: {
     "no-console": ["error", { allow: ["warn", "info", "error"] }],
