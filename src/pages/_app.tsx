abeb12ed1339e3c31e0827f8f77e027ef4b1232a
Raza <raza.elahi@paypay-corp.co.jp>
2022-08-07 06:23:57 +0900

    Font Awesome setup.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import "src/styles/global.scss";
 
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 import { NextUIProvider } from "@nextui-org/react";
 import type { AppProps } from "next/app";
 
@@ -10,6 +12,8 @@ const App = (props: AppProps) => {
     </NextUIProvider>
   );
 };
+
+config.autoAddCss = false
 
 // eslint-disable-next-line import/no-default-export
 export default App;
