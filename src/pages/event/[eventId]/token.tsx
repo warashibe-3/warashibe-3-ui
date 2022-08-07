72fc1dc4e7dab550c1ef3549454b9c22618ca9ae
mandbjp <mandbjp@users.noreply.github.com>
2022-08-07 20:00:09 +0900

    show token hash

--- src/pages/event/[eventId]/token.tsx
+++ src/pages/event/[eventId]/token.tsx
@@ -1,12 +1,42 @@
 import { Grid, Spacer } from "@nextui-org/react";
+import axios from "axios";
 import Router, { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { BaseLayout as Layout } from "src/components/BaseLayout/baselayout";
 import { RockButton } from "src/components/RockButton/rockbutton";
 import { TokenHash } from "src/components/TokenHash/tokenhash";
 
+interface TokenMint {
+  status: number
+  msg: string
+  txHash: string
+}
+
+const mintToken = async (): Promise<TokenMint> => {
+  const params = {
+    amount: 10,
+    target: 'system',
+  }
+  const tokenType = 'st_feedback_points'
+  const {data} = await axios.get(`/api/v1/blockchain/tokens/${tokenType}/mint?amount=10&target=system`, { params })
+  return {
+    status: data.status,
+    msg: data.msg,
+    txHash: data.tx_hash,
+  }
+}
+
+
 const TokenPage = () => {
   const router = useRouter();
   const { eventId } = router.query;
+  const [token, setToken] = useState("ThiSISaTokeNvAlUEAndItsvEryCool");
+
+  useEffect(() => {
+    mintToken().then((ret: TokenMint) => {
+      setToken(ret.txHash)
+    })
+  }, []);
 
   return (
     <Layout
@@ -20,7 +50,7 @@ const TokenPage = () => {
       <Grid direction="column" alignItems="center">
         <Grid>
           <Spacer y={5} />
-          <TokenHash token="ThiSISaTokeNvAlUEAndItsvEryCool" />
+          <TokenHash token={token} />
         </Grid>
         <Spacer y={3} />
         <Grid>
