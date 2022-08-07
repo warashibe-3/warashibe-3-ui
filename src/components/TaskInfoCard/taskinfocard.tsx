04db3093e349c70b2fd85568b64a12298abee49d
Raza <raza.elahi@paypay-corp.co.jp>
2022-08-07 15:41:17 +0900

    Add Assignment Page.

---
+++ src/components/TaskInfoCard/taskinfocard.tsx
@@ -0,0 +1,22 @@
+import { Icon } from "@iconify/react";
+import { Card, Text } from "@nextui-org/react";
+
+import styles from "./taskinfocard.module.css";
+
+export const TaskInfoCard = (
+  { icon, title, description }: { icon: string, title: string, description: string }
+) => {
+  return (
+    <Card variant="flat" className={styles.item}>
+      <Card.Body>
+        <Icon icon={icon} fontSize={"calc(120px + 10 * (100vw - 800px)/100)"} className={styles.icon} />
+      </Card.Body>
+      <Card.Header className={styles.header}>
+        <Text className={styles.title}>{title}</Text>
+      </Card.Header>
+      <Card.Footer className={styles.footer}>
+        <Text className={styles.description}>{description}</Text>
+      </Card.Footer>
+    </Card>
+  );
+};
