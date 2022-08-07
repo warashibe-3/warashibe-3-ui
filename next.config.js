85c59c94d0471d6afdd80caa31006b067f17fc8a
mandbjp <mandbjp@users.noreply.github.com>
2022-08-07 15:58:25 +0900

    Add reverse proxy settings to `next.config.js` (#17)

--- next.config.js
+++ next.config.js
@@ -1,5 +1,26 @@
+const rewrites = () => {
+  const host = "ec2-54-168-156-21.ap-northeast-1.compute.amazonaws.com";
+  const javaBase = `http://${host}:8080`;
+  const pythonBase = `http://${host}:8001`;
+  return [
+    {
+      source: "/api/v1/:path*",
+      destination: `${pythonBase}/api/v1/:path*`,
+    },
+    {
+      source: "/api/:path*",
+      destination: `${javaBase}/:path*`,
+    },
+    {
+      source: "/hello",
+      destination: `${pythonBase}/hello`,
+    },
+  ];
+};
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  rewrites,
   reactStrictMode: true,
   poweredByHeader: false,
   pageExtensions: ["mdx", "md", "jsx", "js", "tsx", "ts"],
