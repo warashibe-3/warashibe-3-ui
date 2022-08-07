e9605f3d010326fa3451662491eafacb449b5956
mandbjp <mandbjp@users.noreply.github.com>
2022-08-07 15:27:17 +0900

    reverse proxy

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
+const rewrites = () => {
+  const base = "http://ec2-54-168-156-21.ap-northeast-1.compute.amazonaws.com:8001";
+  return [
+    {
+      source: "/api/v1/:path*",
+      destination: `${base}/api/v1/:path*`,
+    },
+    {
+      source: "/hello",
+      destination: `${base}/hello`,
+    },
+  ];
+};
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  rewrites,
   reactStrictMode: true,
   poweredByHeader: false,
   pageExtensions: ["mdx", "md", "jsx", "js", "tsx", "ts"],
